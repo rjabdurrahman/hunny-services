54210d5473aacf8218eba07da2f1cbccd24a6a17
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-05-03 14:55:40 +0600

    Added: 404 page not found height

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/admin', (req, res) => {
+    res.render('pages/admin');
+})
+
+app.get('/*', (req, res) => {
+    res.render('pages/404');
+})
+
 const server = app.listen(process.env.PORT || 3000, () => console.log(`Listening on http://localhost:${server.address().port}`));
