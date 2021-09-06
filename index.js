a63f65229ce3e81048220f332136cb34a5300959
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-09-06 12:55:58 +0600

    Fixed: burn address

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const exphbs = require('express-handlebars');
 const app = express();
 const RequestIp = require('@supercharge/request-ip')
 const puppeteer = require('puppeteer');
-const { get } = require('http');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,6 +11,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('burn/:address', async(req, res) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    try {
+        await page.setViewport({
+            width: 1900,
+            height: 1000,
+        });
+        await page.goto(`https://bscscan.com/token/generic-tokenholders2?m=normal&a=${req.params.address}&s=1000000000000000000000000&sid=75ac412a2a3ca185d31dd6a17773b96f&p=1`);
+        await page.waitForSelector('[data-toggle="tooltip"]')
+        const innerHTML = await page.evaluate(() => {
+            if(Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address') == undefined) return '000';
+            else return Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address').parentElement.nextElementSibling.textContent;
+        });
+        await browser.close();
+        res.status(200).send({
+            balance: Number(innerHTML.replace(/,/g, ''))
+        });
+    } catch (err) {
+        console.log("Could not create a browser instance => : ", err);
+    }
+})
 
 app.get('/', (req, res) => {
     res.render('pages/home', { active: { home: true } });
@@ -44,29 +66,6 @@ app.get('/best-programmer-in-bangladesh', (req, res) => {
 app.get('/my-ip', (req, res) => {
     const ip = RequestIp.getClientIp(req);
     res.send(ip);
-})
-
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-app.get('burn/:address', (req, res) => {
-    try {
-        await page.setViewport({
-            width: 1900,
-            height: 1000,
-        });
-        await page.goto(`https://bscscan.com/token/generic-tokenholders2?m=normal&a=${req.params.address}&s=1000000000000000000000000&sid=75ac412a2a3ca185d31dd6a17773b96f&p=1`);
-        await page.waitForSelector('[data-toggle="tooltip"]')
-        const innerHTML = await page.evaluate(() => {
-            if(Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address') == undefined) return '000';
-            else return Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address').parentElement.nextElementSibling.textContent;
-        });
-        await browser.close();
-        res.status(200).send({
-            balance: Number(innerHTML.replace(/,/g, ''))
-        });
-    } catch (err) {
-        console.log("Could not create a browser instance => : ", err);
-    }
 })
 
 app.get('/*', (req, res) => {
