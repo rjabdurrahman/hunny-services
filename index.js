3e0d66e5dbdea8d2b59fdcfa522f594bcc79d293
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-09-06 14:08:44 +0600

    Handled: unkonown parms

--- index.js
+++ index.js
@@ -15,28 +15,35 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/burn/:address', async(req, res) => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox']
-      });
-    const page = await browser.newPage();
-    try {
-        await page.setViewport({
-            width: 1900,
-            height: 1000,
+    if(!address == 'undefined') {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+          });
+        const page = await browser.newPage();
+        try {
+            await page.setViewport({
+                width: 1900,
+                height: 1000,
+            });
+            await page.goto(`https://bscscan.com/token/generic-tokenholders2?m=normal&a=${req.params.address}&s=1000000000000000000000000&sid=75ac412a2a3ca185d31dd6a17773b96f&p=1`);
+            await page.waitForSelector('[data-toggle="tooltip"]')
+            const innerHTML = await page.evaluate(() => {
+                if(Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address') == undefined) return '000';
+                else return Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address').parentElement.nextElementSibling.textContent;
+            });
+            await browser.close();
+            res.status(200).send({
+                balance: Number(innerHTML.replace(/,/g, ''))
+            });
+        } catch (err) {
+            console.log("Could not create a browser instance => : ", err);
+        }
+    }
+    else {
+        res.send({
+            balance: 0
         });
-        await page.goto(`https://bscscan.com/token/generic-tokenholders2?m=normal&a=${req.params.address}&s=1000000000000000000000000&sid=75ac412a2a3ca185d31dd6a17773b96f&p=1`);
-        await page.waitForSelector('[data-toggle="tooltip"]')
-        const innerHTML = await page.evaluate(() => {
-            if(Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address') == undefined) return '000';
-            else return Array.from(document.querySelectorAll('[data-toggle="tooltip"]')).find(x => x.textContent=='Burn Address').parentElement.nextElementSibling.textContent;
-        });
-        await browser.close();
-        res.status(200).send({
-            balance: Number(innerHTML.replace(/,/g, ''))
-        });
-    } catch (err) {
-        console.log("Could not create a browser instance => : ", err);
     }
 })
 
